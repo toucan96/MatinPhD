50a56024b71f8ee128321e1af9aadd5063723ae4
toucan96 <m.mahmoudi@roslin.ed.ac.uk>
2025-02-07 11:20:24 +0100

    group_t_test redundant; ht_cont given own function file

--- functions.R
+++ functions.R
@@ -1,21 +1,3 @@
-ht_cont <- function(data, group_col = 2, vars, FUN = t.test, ...) {
-  n<-1
-  collate <- c()
-  vars <- colnames(data[,vars])
-  repeat {
-    res <- tidy(FUN(data[,vars[n]] ~ data[,group_col], ...)) %>% as.data.frame()
-    res[,"variable"] <- vars[n]
-    collate <- rbind(collate, res)
-    n<-n+1
-    if(n==length(vars)+1) break
-  }
-  return(collate %>% relocate(variable) %>%
-           transform(p.adj=p.adjust(p.value, method="BH")) %>%
-           transform(sign=case_when(p.adj < 0.05 ~ "**", p.value<0.05~"*", p.value>=0.05 ~ ""))
-  )
-}
-
-
 httable <- function(data, group_col=2, vars, threshold=5) {
   n<-1
   collate <- list()
