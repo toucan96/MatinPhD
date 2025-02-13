0169857d24a10838dcbc06be7036b1f43b3b43f8
toucan96 <112857853+toucan96@users.noreply.github.com>
2025-02-13 14:24:10 +0100

    Create volcano.R
    easy volcano plot

---
+++ R/volcano.R
@@ -0,0 +1,20 @@
+volcano <- function(data, x, y, alpha=0.05, min=1) {
+  ggplot(data,
+         aes(
+           x= data[[x]],
+           y= -log10(data[[y]]),
+           colour = 
+             ifelse(data[[y]] < alpha & data[[x]] > min |
+                      data[[y]] < alpha & data[[x]] < - min, "#E7B100", 
+                    ifelse(data[[y]] < alpha, "#006F94", "lightgrey")
+             )
+         )) +
+    geom_point() +
+    scale_colour_identity()+
+    scale_x_continuous(limits = c(-max(sqrt(data[[x]]^2)), max(sqrt(data[[x]]^2)))) +
+    geom_hline(yintercept = -log10(alpha), linetype =2) +
+    geom_vline(xintercept = c(-min,min), linetype =2) +
+    theme_classic()+
+    theme(legend.position = "none") 
+}
+
